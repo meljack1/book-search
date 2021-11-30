422896b9ef71d996498852c20ebcf6fdfeecc094
Mel Jack <melissa_jack@hotmail.co.uk>
2021-11-30 13:37:15 +0000

    Add typeDefs for all schema, queries and mutations

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,46 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+    type Book {
+        _id: ID
+        authors: [String]
+        description: String
+        bookId: ID
+        image: String
+        link: String
+        title: String
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        savedBooks: [Book]
+    }
+
+    type BookInput {
+        authors: [String]
+        description: String
+        bookId: ID
+        image: String
+        link: String
+        title: String
+    }
+
+    type Auth { 
+        token: ID
+        user: User
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!) : Auth
+        login(email: String!, password: String!) : Auth
+        addBook(input: BookInput) : User
+        removeBook(bookId: ID!) : User
+    }
+`
